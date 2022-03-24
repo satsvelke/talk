967b176fb6f701a205a579e3e462077c5600c862
Satish Patil <satsvelke@gmail.com>
2022-03-24 09:58:52 +0530

    Update and rename talk6.js to talk7.js

--- talk7.js
+++ talk7.js
@@ -10,7 +10,7 @@ var domain = 'https://talk.raphacure.co.in/'
 var api = domain.concat('api/');
 
 $(function () {
-    $talk("https://raw.githubusercontent.com/satsvelke/talk/main/chat.html", function () {
+    $talk("https://cdn.jsdelivr.net/gh/satsvelke/talk@main/chat.html", function () {
 
         var groups = [];
         var selectedGroup = {};
